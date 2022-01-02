fff5e2ac2d785f2678874ec34f5eec664ec7a8e0
z80sparrow <sparrow@oct.stars.ne.jp>
2022-01-02 19:14:20 +0900

    Temporary.

---
+++ src/main/java/jp/prototype/common/TestUtil.java
@@ -0,0 +1,12 @@
+package jp.prototype.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestUtil {
+
+  public static void println(String msg) {
+    System.out.println(msg);
+  }
+
+}
